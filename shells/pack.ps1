3a009440727e994c7aca7f5a597043498ff2d2bc
WuZhen <498721344@qq.com>
2022-10-24 21:46:33 +0800

    优化打包发布

--- shells/pack.ps1
+++ shells/pack.ps1
@@ -1,11 +1,9 @@
-pyinstaller -F -n SUDA-Net-Daemon -i resources/suda-logo.png -p dist ./daemon.py
-sleep 1
+pyinstaller -F -n SUDA-Net-Daemon -i resources\suda-logo.png -p dist .\daemon.py
 COPY configurations.json dist\configurations.json
 COPY chromedriver.exe dist\chromedriver.exe
-sleep 1
 $compress = @{
-    Path = "configurations.json", "chromedriver.exe", "SUDA-Net-Daemon.exe"
+    Path = "configurations.json", "chromedriver.exe", "dist\SUDA-Net-Daemon.exe"
     CompressionLevel = "Fastest"
     DestinationPath = "dist\SUDA-Net-Daemon-v0.2.zip"
 }
-Compress-Archive @compress
+Compress-Archive @compress -Force
